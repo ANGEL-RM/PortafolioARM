2bcc3246df0bbb1b35e7c927347cbe58389d1b2b
ANGEL RM <angel_ruiz.m@hotmail.com>
2022-02-08 19:06:45 -0600

    Menu de navegacion

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('#menu-btn');
+const menu = document.querySelector('.menu');
+const detalles = document.querySelector('.detalle');
+btn.addEventListener('click', e=>{
+	menu.classList.toggle("menu-collapsed");
+	menu.classList.toggle("menu-expanded");	
+    document.querySelector('body').classList.toggle('body-expanded');
+    document.querySelector('#descripcion').classList.toggle('mostrartitle');
+    btn.classList.toggle('open');
+});
